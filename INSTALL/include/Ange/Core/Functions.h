36097e14ce275db72efc6b5962009aea69604572
Arkadiusz Jarlaczyk <arekjar@gmail.com>
2019-08-23 00:48:41 +0200

    Updated .gitignore

--- INSTALL/include/Ange/Core/Functions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef ANGE_PLATFORM_WINDOWS
-#include "Ange/Platform/Win32/Functions.h"
-#elif ANGE_PLATFORM_LINUX
-#include "Ange/Platform/Linux/Functions.h"
-#endif
--- INSTALL/include/Apch.cpp
+++
@@ -1,1 +0,0 @@
-#include "Apch.h"
